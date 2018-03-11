c893e87e26d91fd946521a830058b95dd0f68190
Cast <cast@nothing.ch>
2018-03-11 16:35:51 +0700

    feat: strip all html from a doc

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # rehype-strip-html
-Strip all HTML from a document using rehype
+
+Strip all HTML from a document using rehype.
+
+## Installation
+
+``` bash
+$ npm add --save rehype-strip-html
+```
+
+## Example Usage
+
+TODO
+
+## Licence
+
+LGPL-v3.0 or later
---
+++ index.js
@@ -0,0 +1,18 @@
+var visit = require("unist-util-visit");
+
+module.exports = stripHtml;
+
+function stripHtml(config) {
+  /* Exposing the compiler function */
+  this.Compiler = compiler;
+
+  function compiler(tree) {
+    let fragments = [];
+    /* We visit each text node, and we push the value in the fragments array */
+    visit(tree, "text", function(node) {
+      fragments.push(node.value);
+    });
+    /* We recompose the final document */
+    return parts.join("");
+  }
+}
